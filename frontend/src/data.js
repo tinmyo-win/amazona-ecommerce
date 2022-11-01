9d8c319169814be4009f874dd37106cfbdc08b8f
Tin Myo Win <tinmyowin356@gmail.com>
2022-11-01 12:48:40 +0630

    Start to create amazon app

---
+++ frontend/src/data.js
@@ -0,0 +1,66 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/product-1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+
+        },
+        {
+            _id: '2',
+            name: 'Adidas fit Shirt',
+            category: 'Shirts',
+            image: '/images/product-2.jpg',
+            price: 160,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 17,
+            description: 'high quality product',
+
+        },
+        {
+            _id: '3',
+            name: 'Gucci Shirt',
+            category: 'Shirts',
+            image: '/images/product-3.jpg',
+            price: 240,
+            brand: 'Gucci',
+            rating: 5,
+            numReviews: 25,
+            description: 'high quality product',
+
+        },
+        {
+            _id: '4',
+            name: 'Nike Pants',
+            category: 'Pants',
+            image: '/images/product-5.jpg',
+            price: 100,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 12,
+            description: 'high quality product',
+
+        },
+        {
+            _id: '6',
+            name: 'Long Pants',
+            category: 'Pants',
+            image: '/images/product-6.jpg',
+            price: 150,
+            brand: 'Nike',
+            rating: 4,
+            numReviews: 19,
+            description: 'high quality product',
+
+        },
+    ]
+}
+
+export default data;
